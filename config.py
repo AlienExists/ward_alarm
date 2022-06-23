231a0d2ec42b6f5afde922f2b5e2b6e506c9f8cf
OldTyT <nezlobin118@gmail.com>
2022-06-23 22:58:51 +0800

    fix depoyment on heroku

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     SQLALCHEMY_MIGRATE_REPO = "db"
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JSON_ADD_STATUS = False
     JSON_DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
